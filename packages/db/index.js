eb8943835a08ab5eb3da1ebbb59f05d1a0422c25
muhammadAB1 <abrarjpj7@gmail.com>
2026-03-19 02:11:30 +0500

    Added vercel configuration

--- packages/db/index.js
+++ packages/db/index.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const prisma = new PrismaClient({
