cfc7267acb9922e5085bbfd73859aa99576f9613
muhammadAB1 <abrarjpj7@gmail.com>
2026-03-26 17:08:21 +0500

    For uploading to vercel

---
+++ apps/backend-api/llm/base.ts
@@ -0,0 +1,19 @@
+import { type Messages } from "../types";
+
+export type LlmResponse = {
+    completions: {
+        choices: {
+            message: {
+                content: string
+            }
+        }[]
+    },
+    inputTokensConsumed: number,
+    outputTokensConsumed: number
+}
+
+export class BaseLlm {
+    static async chat(model: string, messages: Messages): Promise<LlmResponse> {
+        throw new Error("Not implemented chat function")
+    }
+}
