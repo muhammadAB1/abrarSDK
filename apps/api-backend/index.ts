e6a60611a6361cd24e743d56457ef7cb48c17e32
muhammadAB1 <abrarjpj7@gmail.com>
2026-04-02 02:00:20 +0500

    for vercel

--- apps/api-backend/index.ts
+++ apps/api-backend/index.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import type { Request, Response } from 'express'
 import type { Messages } from './types';
 import { Gemini } from './llm/Gemini';
 // import { OpenAi } from './llm/OpenAi';
@@ -10,12 +9,12 @@ import type { LlmResponse } from './llm/base';
 const app = express();
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: any, res: any) => {
     res.json({ message: "Hello World" })
 })
 
 
-app.post('/conversation', async (req: Request, res: Response) => {
+app.post('/conversation', async (req: any, res: any) => {
     const { model, messages }: { model: string, messages: Messages } = req.body;
     const [companyName, provideModelName] = model.split('/');
 
